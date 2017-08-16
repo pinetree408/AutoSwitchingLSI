4b7eb394f39804b792fdeb0c8ddb81d0e345feae
leesangyoon <pinetree408@gmail.com>
2017-08-16 21:10:51 +0900

    Add first commit

---
+++ app/src/main/java/com/pinetree408/research/watchtapboard/MainActivity.java
@@ -0,0 +1,318 @@
+package com.pinetree408.research.watchtapboard;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Gesture Variable
+    private int dragThreshold = 30;
+    private final double angleFactor = (double) 180/Math.PI;
+
+    private long touchDownTime;
+    private float touchDownX, touchDownY;
+    private float scrollY;
+
+    // View Variable
+    ArrayList<String> originSourceList;
+    ArrayList<String> sourceList;
+
+    ArrayAdapter<String> adapter;
+    ListView listview;
+
+    TextView tv;
+    TapBoardView tapBoardView;
+
+    String inputString;
+    View keyboardContainer;
+
+    // Task Variable
+    Random random;
+    String target;
+    // 0 : tapboard
+    // 1 : result based
+    // 2 : input based
+    int keyboardMode = 2;
+    Toast toast;
+    TextView startView;
+    View taskView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        random = new Random();
+        initSourceList();
+
+        listview = (ListView) findViewById(R.id.list_view);
+        tv = (TextView) findViewById(R.id.place_holder);
+        tapBoardView = (TapBoardView) findViewById(R.id.tapboard);
+        keyboardContainer = (View) findViewById(R.id.keyboard_container);
+
+        startView = (TextView) findViewById(R.id.start);
+        taskView = findViewById(R.id.task);
+
+        initListView();
+        initKeyboardContainer();
+        initStartView();
+
+        startView.setVisibility(View.VISIBLE);
+        taskView.setVisibility(View.GONE);
+    }
+
+    public void initSourceList() {
+        sourceList = new ArrayList<String>();
+        originSourceList = new ArrayList<String>();
+        for (int i = 0; i < Source.set1.length; i++) {
+            sourceList.add(Source.set1[i]);
+        }
+        for (int i = 0; i < Source.set2.length; i++) {
+            sourceList.add(Source.set2[i]);
+        }
+        for (int i = 0; i < Source.set3.length; i++) {
+            sourceList.add(Source.set3[i]);
+        }
+        originSourceList.addAll(sourceList);
+    }
+
+    public void initListView() {
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sourceList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+                tv.setTextColor(Color.BLACK);
+                tv.setBackgroundColor(Color.WHITE);
+                tv.setGravity(Gravity.CENTER);
+                tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                return view;
+            }
+        };
+        listview.setAdapter(adapter);
+        listview.setBackgroundColor(Color.WHITE);
+    }
+
+    public void initKeyboardContainer() {
+        inputString = "";
+        keyboardContainer.bringToFront();
+        if (keyboardMode != 0) {
+            keyboardContainer.setBackgroundColor(Color.WHITE);
+        }
+        keyboardContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                int tempX = (int) event.getAxisValue(MotionEvent.AXIS_X);
+                int tempY = (int) event.getAxisValue(MotionEvent.AXIS_Y);
+                long eventTime = System.currentTimeMillis();
+
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        touchDownTime = eventTime;
+                        touchDownX = tempX;
+                        touchDownY = tempY;
+                        scrollY = tempY;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int moveLength = (int) scrollY - tempY;
+                        listview.smoothScrollBy(moveLength, 0);
+                        scrollY = tempY;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        long touchTime = eventTime - touchDownTime;
+                        int xDir = (int) (touchDownX - tempX);
+                        int yDir = (int) (touchDownY - tempY);
+                        int len = (int) Math.sqrt(xDir * xDir + yDir * yDir);
+                        int speed;
+                        if (touchTime > 0) {
+                            speed = (int) (len * 1000 / touchTime);
+                        } else {
+                            speed = 0;
+                        }
+                        if (len > dragThreshold) {
+                            if (speed > 400) {
+                                double angle = Math.acos((double) xDir / len) * angleFactor;
+                                if (yDir < 0) {
+                                    angle = 360 - angle;
+                                }
+                                angle += 45;
+                                int id = (int) (angle / 90);
+                                if (id > 3) {
+                                    id = 0;
+                                }
+                                switch (id){
+                                    case 0:
+                                        //left
+                                        inputString = "";
+                                        tv.setText(inputString);
+                                        setResultAtListView(inputString);
+                                        if (keyboardMode != 0) {
+                                            tv.setVisibility(View.VISIBLE);
+                                            tapBoardView.setVisibility(View.VISIBLE);
+                                            keyboardContainer.setBackgroundColor(Color.WHITE);
+                                        }
+                                        break;
+                                    case 1:
+                                        //top;
+                                        break;
+                                    case 2:
+                                        //right
+                                        for (int i = 0; i < listview.getChildCount(); i++) {
+                                            TextView childView = (TextView) listview.getChildAt(i);
+                                            boolean under = touchDownY <= (childView.getY() + childView.getHeight());
+                                            boolean over = touchDownY >= childView.getY();
+                                            if (under && over) {
+                                                if (childView.getText().equals(target)) {
+                                                    target = originSourceList.get(random.nextInt(originSourceList.size()));
+                                                    startView.setText(target);
+                                                    startView.setVisibility(View.VISIBLE);
+                                                    taskView.setVisibility(View.GONE);
+                                                    inputString = "";
+                                                    tv.setText(inputString);
+                                                    setResultAtListView(inputString);
+                                                    if (keyboardMode != 0) {
+                                                        tv.setVisibility(View.VISIBLE);
+                                                        tapBoardView.setVisibility(View.VISIBLE);
+                                                        keyboardContainer.setBackgroundColor(Color.WHITE);
+                                                    }
+                                                } else {
+                                                    childView.setBackgroundColor(Color.RED);
+                                                }
+                                                break;
+                                            }
+                                        }
+                                        break;
+                                    case 3:
+                                        //bottom;
+                                        break;
+                                }
+                            }
+                        } else {
+                            if (tapBoardView.getVisibility() == View.VISIBLE) {
+                                if (touchTime < 100) {
+                                    String[] params = getInputInfo(event);
+                                    if (params[0].equals(".")) {
+                                        break;
+                                    }
+                                    inputString += params[0];
+                                    tv.setText(inputString);
+                                    setResultAtListView(inputString);
+                                    if (keyboardMode == 2) {
+                                        if (inputString.length() > 2) {
+                                            if (sourceList.size() != 0) {
+                                                tv.setVisibility(View.GONE);
+                                                tapBoardView.setVisibility(View.GONE);
+                                                keyboardContainer.setBackgroundColor(Color.parseColor("#00000000"));
+                                            } else {
+                                                if (toast != null) {
+                                                    toast.cancel();
+                                                }
+                                                toast = Toast.makeText(getApplicationContext(), "There are no itmes", Toast.LENGTH_SHORT);
+                                                toast.show();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void initStartView() {
+        target = originSourceList.get(random.nextInt(originSourceList.size()));
+        startView.setText(target);
+        startView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        runOnUiThread(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    startView.setVisibility(View.GONE);
+                                    taskView.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        );
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void setResultAtListView(String inputString) {
+        if (inputString.equals("")) {
+            sourceList.clear();
+            for (int i = 0; i < Source.set1.length; i++) {
+                sourceList.add(Source.set1[i]);
+            }
+            for (int i = 0; i < Source.set2.length; i++) {
+                sourceList.add(Source.set2[i]);
+            }
+            for (int i = 0; i < Source.set3.length; i++) {
+                sourceList.add(Source.set3[i]);
+            }
+        } else {
+            ArrayList<String> tempList = new ArrayList<String>();
+            for (String item : sourceList) {
+                if (item.startsWith(inputString)) {
+                    tempList.add(item);
+                }
+            }
+            sourceList.clear();
+            sourceList.addAll(tempList);
+        }
+        adapter.notifyDataSetChanged();
+        listview.setSelectionAfterHeaderView();
+
+        if (keyboardMode == 1) {
+            if (sourceList.size() <= 10) {
+                if (sourceList.size() != 0) {
+                    tv.setVisibility(View.GONE);
+                    tapBoardView.setVisibility(View.GONE);
+                    keyboardContainer.setBackgroundColor(Color.parseColor("#00000000"));
+                } else {
+                    if (toast != null) {
+                        toast.cancel();
+                    }
+                    toast = Toast.makeText(getApplicationContext(), "There are no itmes", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        }
+    }
+
+    public String[] getInputInfo(MotionEvent event) {
+        double tempX = (double) event.getAxisValue(MotionEvent.AXIS_X);
+        double tempY = (double) event.getAxisValue(MotionEvent.AXIS_Y);
+
+        String input = tapBoardView.getKey(tempX, tempY - tapBoardView.getY());
+
+        String[] params = {
+                String.valueOf(input),
+                String.valueOf(tempX),
+                String.valueOf(tempY)
+        };
+        return params;
+    }
+}
---
+++ app/src/main/java/com/pinetree408/research/watchtapboard/TapBoardView.java
@@ -0,0 +1,143 @@
+package com.pinetree408.research.watchtapboard;
+
+/**
+ * Created by leesangyoon on 2017. 8. 15..
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class TapBoardView extends View {
+
+    char[] keyboardCharList = {
+            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+            'z', 'x', 'c', 'v', 'b', 'n', 'm'
+    };
+
+    char[][] keyboardChar = {
+            {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'},
+            {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'},
+            {'z', 'x', 'c', 'v', 'b', 'n', 'm'}
+    };
+
+    List<String> keyboardCharPos;
+    double keyWidthRef;
+    double keyHeightRef;
+
+    Paint keyboardPaint;
+
+    public TapBoardView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public TapBoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public TapBoardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public String getKey(double x, double y) {
+        int id = -1;
+
+        for (int i = 0; i < keyboardCharPos.size(); i++) {
+            String pos = keyboardCharPos.get(i);
+            double posX = Double.parseDouble(pos.split("-")[0]);
+            double posY = Double.parseDouble(pos.split("-")[1]);
+            if ((Math.abs(posX - x) <= keyWidthRef) && (Math.abs(posY - y) <= keyHeightRef)) {
+                id = i;
+                break;
+            }
+        }
+
+        if (id == -1) {
+            return ".";
+        }
+
+        return String.valueOf(keyboardCharList[id]);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.TapBoardView, defStyle, 0);
+        a.recycle();
+
+        keyboardPaint = new Paint();
+        keyboardPaint.setAntiAlias(true);
+        keyboardPaint.setTextAlign(Paint.Align.CENTER);
+
+        keyboardCharPos = new ArrayList<String>();
+        keyHeightRef = -1.0;
+        keyWidthRef = -1.0;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        double viewWidth = getWidth();
+        double viewHeight = getHeight();
+
+        double keyWidth = viewWidth / 10;
+        //double keyHeight = viewHeight / 4;
+        double keyHeight = keyWidth * 1.5;
+
+        keyboardPaint.setTextSize((float) (keyHeight * 0.9));
+        keyboardPaint.setColor(Color.parseColor("#80000000"));
+
+        if (keyWidthRef == -1.0) {
+            keyWidthRef = keyWidth * 0.5;
+        }
+        if (keyHeightRef == -1.0) {
+            keyHeightRef = keyHeight * 0.5;
+        }
+
+        double keyboardPaddingLeft = (viewWidth - (10 * keyWidth)) * 0.5;
+        //double keyboardPaddingTop = 0;
+        double keyboardPaddingTop = (viewHeight - (3 * keyHeight)) * 0.4;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j < keyboardChar[i].length) {
+                    String content = String.valueOf(keyboardChar[i][j]);
+
+                    float leftPadding = (float) (keyboardPaddingLeft + j * keyWidth + (keyWidth * 0.5));
+                    float topPadding = (float) (keyboardPaddingTop + i * keyHeight + (keyHeight * 0.5));
+                    if (i == 1) {
+                        leftPadding += keyWidth * 0.5;
+                    } else if (i == 2) {
+                        leftPadding += keyWidth * 1.0;
+                    }
+
+                    if (keyboardCharPos.size() != 26) {
+                        keyboardCharPos.add(leftPadding + "-" + topPadding);
+                    }
+
+                    canvas.drawText(content,
+                            leftPadding,
+                            topPadding,
+                            keyboardPaint);
+                }
+            }
+        }
+
+    }
+
+}
